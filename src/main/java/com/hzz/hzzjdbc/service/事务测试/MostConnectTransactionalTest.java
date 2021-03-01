4fc63476b8065d09c6697b7951da2dbc6cf61b0e
huzz <605198347@qq.com>
2021-03-01 16:53:22 +0800

    多数据库连接事务加入完成，但是注入方式较为麻烦，且注入的Mysqldao名字固定

--- src/main/java/com/hzz/hzzjdbc/service/事务测试/MostConnectTransactionalTest.java
+++ src/main/java/com/hzz/hzzjdbc/service/事务测试/MostConnectTransactionalTest.java
@@ -6,7 +6,6 @@ import com.hzz.hzzjdbc.jdbcutil.searchmain.MysqlUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author ：hzz
@@ -30,7 +29,7 @@ public class MostConnectTransactionalTest {
 
 
     /*注释这边还不能支持多数据源的事务，仅能用封装的方法进行*/
-    @Transactional(rollbackFor = Exception.class)
+    //@Transactional(rollbackFor = Exception.class)
     @TransactionalMostConnect(DataSourcesNames ={"mysqldata2","mysqldata","mysqldata3"},rollbackFor = Exception.class)
     public void testrollback() {
         MysqlUtil devmysqlUtil = mysqldata2.getMysqlUtil();
@@ -40,7 +39,7 @@ public class MostConnectTransactionalTest {
 
         MysqlUtil dev45mysqlUtil = mysqldata3.getMysqlUtil();
         dev45mysqlUtil.excutesql("update department set name ='测试11' where depId='117445961' ");
-      //  throw  new RuntimeException("");
+        throw  new RuntimeException("");
     }
 
 
