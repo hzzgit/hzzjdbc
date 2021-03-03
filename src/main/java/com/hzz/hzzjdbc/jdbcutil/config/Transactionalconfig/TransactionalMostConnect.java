bc0d95f115c1d83f22f9288c2f155e4a5d727d1b
huzz <605198347@qq.com>
2021-03-03 15:02:48 +0800

    加入注解使用的类和对应方法的注释

--- src/main/java/com/hzz/hzzjdbc/jdbcutil/config/Transactionalconfig/TransactionalMostConnect.java
+++ src/main/java/com/hzz/hzzjdbc/jdbcutil/config/Transactionalconfig/TransactionalMostConnect.java
@@ -1,9 +1,10 @@
 package com.hzz.hzzjdbc.jdbcutil.config.Transactionalconfig;
 
+import org.springframework.cglib.proxy.MethodProxy;
 import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.annotation.Propagation;
 
 import java.lang.annotation.*;
+import java.lang.reflect.Method;
 import java.sql.Connection;
 
 /**
@@ -18,24 +19,25 @@ import java.sql.Connection;
 public @interface TransactionalMostConnect {
     /**
      * 设置事务传播级别
-     * <p>Defaults to {@link Propagation#REQUIRED}.
-     * @see org.springframework.transaction.interceptor.TransactionAttribute#getPropagationBehavior()
+     * <p>Defaults to {@link Connection#TRANSACTION_REPEATABLE_READ}.
+     * @see com.hzz.hzzjdbc.jdbcutil.searchmain.SearchExecuter#begintransaction(int, boolean) ()
      */
     int TransactionIsolation() default Connection.TRANSACTION_REPEATABLE_READ;
     /**
      * 设置超时时间(s)
+     * @see TransactionalInterceptor#intercept(Object, Method, Object[], MethodProxy)
      */
     int timeout() default TransactionDefinition.TIMEOUT_DEFAULT;
     /**
-     * 设置捕获到的异常进行回滚，不配置则所有异常都回复你
-     * @return
+     * 设置捕获到的异常进行回滚，不配置则所有异常都回滚
+     * @see TransactionalInterceptor#intercept(Object, Method, Object[], MethodProxy)
      */
     Class<? extends Throwable>[] rollbackFor() default {};
 
     /**
      * 可指定数据源进行回滚，不配置则所有数据库都回滚，主数据库名称为mysqlDao
      * 其他数据库  如 spring.datasource.mysql96.driverClassName 则名为mysql96
-     *
+     * @see TransactionalInterceptor#intercept(Object, Method, Object[], MethodProxy)
      * @return
      */
     String[] DataSourcesNames() default {};
