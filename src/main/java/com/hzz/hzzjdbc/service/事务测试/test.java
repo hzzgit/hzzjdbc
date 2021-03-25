500627686d353d36ecef371879591ebe8b918ac0
huzz <605198347@qq.com>
2021-03-25 09:59:37 +0800

    加入注解使用的类和对应方法的注释

--- src/main/java/com/hzz/hzzjdbc/service/事务测试/test.java
+++ src/main/java/com/hzz/hzzjdbc/service/事务测试/test.java
@@ -2,7 +2,8 @@ package com.hzz.hzzjdbc.service.事务测试;
 
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class test {
@@ -10,11 +11,19 @@ public class test {
 
 
     public static void main(String[] args) {
+        Map<Key, Integer> map = new HashMap<>();
+        for (int i = 0; i < 1000; i++) {
+            map.put(new Key(), i);
+        }
 
-        Class<Student> studentClass = Student.class;
-        Method[] methods = studentClass.getMethods();
-        for (Method method : methods) {
-            
+        // 断点打折这里
+        System.out.println(map);
+    }
+
+    static class Key {
+        @Override
+        public int hashCode() {
+            return 1;
         }
     }
 }
